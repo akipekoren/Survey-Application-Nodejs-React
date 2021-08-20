899e99fe9ac491ddb6da211519a1bbca244d7b03
Ahmet Kaan Ipekoren <s287772@studenti.polito.it>
2021-08-20 22:40:16 +0300

    deployment

--- server.js
+++ server.js
@@ -173,6 +173,18 @@ app.get("/api/sessions/current", (req, res) => {
   } else res.status(401).json({ error: "Unauthenticated user!" });
 });
 
+// Serve static assets if in production
+
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 app.listen(port, () =>
   console.log(`Server running on http://localhost:${port}/`)
 );
